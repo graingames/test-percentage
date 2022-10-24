696f0d96be64b6fd69fdf5105b283e9c0cc6b7f6
graingames <98623348+graingames@users.noreply.github.com>
2022-10-24 17:04:50 +0100

    Added Project
    Uploading all my projects, this one created on 26.07.22

---
+++ TestPercentage.py
@@ -0,0 +1,4 @@
+Total=float(input("How many marks was the test out off?(Total Marks) "))
+Marks=float(input("How many marks did you get? "))
+Percentage=(Marks/Total)*100
+input("Your percentage rounded is "+str(round(Percentage))+", your unrounded percentage is "+str(Percentage))
